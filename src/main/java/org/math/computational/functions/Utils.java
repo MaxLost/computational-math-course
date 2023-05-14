5ad14dd1b4603da7dd4ba2e6dd0bb27cf2848e03
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-05-14 15:43:16 +0300

    Added methods for 2nd and 3rd derivatives

--- src/main/java/org/math/computational/functions/Utils.java
+++ src/main/java/org/math/computational/functions/Utils.java
@@ -11,4 +11,15 @@ public class Utils {
 		return computeDerivative(point, function, h);
 	};
 
+	public static double computeSecondDerivative(double point, Function function) {
+		double h = 1e-8;
+		return (function.evaluate(point + h) - 2* function.evaluate(point) + function.evaluate(point - h)) / (h * h);
+	}
+
+	public static double computeThirdDerivative(double point, Function function) {
+		double h = 1e-8;
+		return (function.evaluate(point + 2*h) - 2*function.evaluate(point + h) +
+				2*function.evaluate(point - h) - function.evaluate(point - 2*h)) / (2 * Math.pow(h, 3));
+	}
+
 }
