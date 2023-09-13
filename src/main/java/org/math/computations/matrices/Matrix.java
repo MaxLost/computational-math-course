ec2c4f97839b7e2bc5c788c8c3c81b2e0610a438
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-09-13 15:21:35 +0300

    Fix

--- src/main/java/org/math/computations/matrices/Matrix.java
+++ src/main/java/org/math/computations/matrices/Matrix.java
@@ -1,10 +1,9 @@
 package org.math.computations.matrices;
 
-
-import java.util.List;
-
-public interface Matrix
-{
+/**
+ * Class for representation of matrices.
+ */
+public interface Matrix {
   double EPSILON = 10e-6;
 
   int[] getSize();
@@ -12,22 +11,22 @@ public interface Matrix
   double getElement(int x, int y);
 
   /**
-   * Single-thread matrix multiplication
-   * <p>
+   * Single-thread matrix multiplication.
+   * <br>
    * (1) A@B = C
    *
-   * @param o - B matrix in (1)
-   * @return - result of matrix multiplication, C matrix in (1)
+   * @param o B matrix in (1)
+   * @return result of matrix multiplication, C matrix in (1)
    */
   Matrix mul(Matrix o);
 
   /**
-   * Multi-thread matrix multiplication
-   * <p>
+   * Multi-thread matrix multiplication.
+   * <br>
    * (1) A@B = C
    *
-   * @param o - B matrix in (1)
-   * @return - result of matrix multiplication, C matrix in (1)
+   * @param o B matrix in (1)
+   * @return result of matrix multiplication, C matrix in (1)
    */
   Matrix dmul(Matrix o);
 }
